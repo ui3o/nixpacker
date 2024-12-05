84ef3d18ffc3ea012d927bee40a6a51a93e66e3e
ui3o.com <ui3o.com@gmail.com>
2024-12-05 22:43:02 +0100

    extra debug removed

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -10,7 +10,6 @@ subprocess.run("cp -rfv /.nix/*.info /nix/info", shell=True)
 packs = [ f.name for f in os.scandir("/nix/warehouse/") if f.is_dir() ]
 links = [ f.name for f in os.scandir("/nix/store/") if f.is_dir() ]
 for dirname in list(packs):
-    print(dirname)
     if dirname not in links:
         print("[INFO] generate symlink for: ", dirname)
         subprocess.run(["ln", "-s", f"/nix/warehouse/{dirname}", f"/nix/store/{dirname}"])
