200cb52d5056e623bab2904fe9822bcd05744c00
ui3o.com <ui3o.com@gmail.com>
2024-12-05 22:01:41 +0100

    nip generation create fix

--- nip/process.py
+++ nip/process.py
@@ -9,48 +9,55 @@ import hashlib
 # print(f"NIP_INSTALL is {os.environ.get('NIP_INSTALL', None)}")
 
 
+def warn(msg: str):
+    print(f"[WARN] {msg}")
+    sys.stdout.flush()
+
+
 def info(msg: str):
     print(f"[INFO] {msg}")
     sys.stdout.flush()
+
+
+def execute(cmd):
+    # info(f"[RUN] {cmd}")
+    subprocess.run(cmd, shell=True)
 
 
 def imagePull(tags: list[str]):
     # check if docker image was pulled
     podman = "/template/podman"
     for tag in tags:
+        image = f"docker.io/ui3o/nixpacker:{tag}"
         if os.path.exists(os.path.expanduser(f"~/.nix/info/{tag}.info")) is False:
-            image = f"docker.io/ui3o/nixpacker:{tag}"
             info(f"pull and run docker image: {image}")
-            subprocess.run(f"{podman} pull {image}", shell=True)
-            subprocess.run(
-                f"{podman} run --privileged --rm -v $OS_HOME/.nix:/nix -it {image}",
-                shell=True,
-            )
+            execute(f"{podman} pull {image}")
+            execute(f"{podman} run --privileged --rm -v $OS_HOME/.nix:/nix -it {image}")
+        else:
+            info(f"this docker image already pulled: {image}")
 
 
 def createGeneration(
     tags: list[str], defaults: dict[str, str], osBindings: dict[str, str]
 ):
     # check if docker image was pulled
-    defToJson = json.dumps(defaults) + json.dumps(osBindings)
+    defToJson = json.dumps(tags) + json.dumps(defaults) + json.dumps(osBindings)
     osHome = os.environ.get("OS_HOME", None)
     md5sum = hashlib.md5(defToJson.encode("utf-8")).hexdigest()
     generationPath = os.path.expanduser(f"~/.nix/generations/{md5sum}")
     if os.path.isdir(generationPath) is False:
         info("create generation")
         imagePull(tags)
-        subprocess.run(
-            f"mkdir -p {generationPath} && cp -r ~/.nix/store/* {generationPath}/",
-            shell=True,
-        )
+        execute(f"mkdir -p {generationPath} && cp -r ~/.nix/store/* {generationPath}/")
+        execute(f"touch {generationPath}/paths")
         for key, value in defaults.items():
             versionArray = value.split("||")
             endOfPath = ""
             version = versionArray[0]
             if len(versionArray) == 2:
                 endOfPath = versionArray[1]
-            allVersionRegex = re.compile(r".{32}-" + key + r"-.+" + endOfPath)
-            taggedVersionRegex = re.compile(r".{32}-" + key + "-" + version + endOfPath)
+            allVersionRegex = re.compile(r".{32}-" + key + r"-.+" + f"{endOfPath}$")
+            taggedVersionRegex = re.compile(r".{32}-" + f"{key}-{version}{endOfPath}$")
             taggedHashedPath = ""
             for item in os.listdir(generationPath):
                 if allVersionRegex.match(item) and taggedVersionRegex.match(item):
@@ -67,45 +74,44 @@ def createGeneration(
                 ).replace(f"{key}-", "")
                 if allVersionRegex.match(item):
                     if taggedVersionRegex.match(item) is None:
-                        info(
-                            f"set {key} version: {currentVersion} -> {version} ({item} -> {taggedHashedPath})"
-                        )
-                        subprocess.run(
-                            f"ln -sf {taggedHashedPath} /nix/store/{item}",
-                            shell=True,
-                        )
+                        if len(taggedHashedPath) > 0:
+                            info(
+                                f"set {key} version: {currentVersion} -> {version} ({item} -> {taggedHashedPath})"
+                            )
+                            execute(f"ln -sf {taggedHashedPath} /nix/store/{item}")
+                        else:
+                            warn(
+                                f"can not set {key} version: {currentVersion} -> {version} ({item} -> {taggedHashedPath})"
+                            )
         # if the new generation create have to make new osBindings
         info("create osBindings into generation folder")
-        subprocess.run(f"mkdir -p {generationPath}/osBindings", shell=True)
-        subprocess.run(f"cp /template/nip {generationPath}/osBindings", shell=True)
+        execute(f"mkdir -p {generationPath}/osBindings")
+        execute(f"cp /template/nip {generationPath}/osBindings")
         for key, value in osBindings.items():
+            info(f"create program on host os: {key}")
             with open(f"{generationPath}/osBindings/{key}", "w") as f:
                 f.write("#!/bin/bash\n")
                 f.write(f"export NIP_BASENAME={value}\n")
-                f.write("nip\n")
-            subprocess.run(f"chmod ugo+x {generationPath}/osBindings/{key}", shell=True)
+                f.write("nip $@\n")
+            execute(f"chmod ugo+x {generationPath}/osBindings/{key}")
 
     # create osBindings on host
-    subprocess.run(
-        f"ln -sf {osHome}/.nix/generations/{md5sum}/osBindings /root/.nix/bin",
-        shell=True,
-    )
+    execute(f"ln -sf {osHome}/.nix/generations/{md5sum}/osBindings /root/.nix/bin")
     # create generation link in container
-    subprocess.run(f"ln -sf {generationPath} /nix/store", shell=True)
+    execute(f"ln -sf {generationPath} /nix/store")
 
 
 def preCheck():
     if os.path.exists("/template/configs/config.py") is False:
         info("create required directories")
-        subprocess.run(
-            "mkdir -p ~/.nix/generations ~/.nix/warehouse ~/.nix/info ~/.nix/store ~/.config/nip",
-            shell=True,
+        execute(
+            "mkdir -p ~/.nix/generations ~/.nix/warehouse ~/.nix/info ~/.nix/store ~/.config/nip"
         )
         info("copy typings into host config folder")
-        subprocess.run("cp /template/configs_tmp/typings.py ~/.config/nip/typings.py", shell=True)
+        execute("cp /template/configs_tmp/typings.py ~/.config/nip/typings.py")
         info("copy config.py into host config folder")
-        subprocess.run("cp /template/configs_tmp/config.py ~/.config/nip", shell=True)
-    
+        execute("cp /template/configs_tmp/config.py ~/.config/nip")
+
     import configs.config as config
 
     # create link generation
@@ -114,7 +120,7 @@ def preCheck():
     )
 
 
-if sys.argv[1] == "nip" or os.environ.get("NIP_INSTALL", None) is not None:
+if sys.argv[1] == "nip":
     preCheck()
 else:
     preCheck()
@@ -124,7 +130,6 @@ else:
         f.write("#!/bin/bash\n")
         f.write(f"export PATH=/template:{os.environ.get("PATH", None)}\n")
         f.write("source /nix/store/paths\n")
-        # TODO hello --version nem ment at
-        f.write("bash\n")
+        # f.write("bash\n")
         f.write(" ".join(sys.argv[1:]) + "\n")
     os._exit(78)
