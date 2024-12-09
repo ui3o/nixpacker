48b6adde35ee97c39a02346950b89c033cba6f32
ui3o.com <ui3o.com@gmail.com>
2024-12-09 18:03:48 +0100

    passwd fix for cwd passthrough

--- nip/nip.sh
+++ nip/nip.sh
@@ -4,7 +4,7 @@
 NIP_CMD="${NIP_BASENAME} $@"
 NIP_RUNTIME=podman
 NIP_RUN_DEFAULTS="run --network=host --rm --privileged -it --env-file $HOME/.nix/environment"
-NIP_RUN_DEFAULTS="${NIP_RUN_DEFAULTS} -v $HOME:$HOME"
+NIP_RUN_DEFAULTS="${NIP_RUN_DEFAULTS} -v $HOME:$HOME -v $HOME:$HOME --mount=type=bind,source=$HOME/.nix/passwd,destination=/etc/passwd"
 [[ $PWD == $HOME* ]] && NIP_RUN_DEFAULTS="${NIP_RUN_DEFAULTS} --workdir $PWD"
 NIP_IMAGE="docker.io/ui3o/nixpacker:nip"
 [ -f ~/.config/nip/.bashrc ] && source ~/.config/nip/.bashrc
@@ -15,7 +15,7 @@ NIP_IMAGE="docker.io/ui3o/nixpacker:nip"
     $NIP_RUNTIME $NIP_RUN_DEFAULTS -v $HOME:/root --entrypoint cp $NIP_IMAGE /template/sshd_config /root/.ssh/nip/sshd_config
 /usr/sbin/sshd -h ~/.ssh/id_rsa -f $HOME/.ssh/nip/sshd_config
 
-[ ! -d ~/.nix ] && mkdir -p ~/.nix
+[ ! -d ~/.nix ] && mkdir -p ~/.nix && echo "root:x:0:0:root:$HOME:/bin/sh" > ~/.nix/passwd 
 env > ~/.nix/environment
 echo "PATH=$PATH" > ~/.ssh/environment
 echo "USER=root" >> ~/.nix/environment
