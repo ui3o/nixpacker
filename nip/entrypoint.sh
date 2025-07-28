5a2fd40f911e6783808f295b09c2af41d0af598e
ui3o.com <ui3o.com@gmail.com>
2025-07-29 00:42:40 +0200

    Refactor entrypoint and nip scripts: streamline directory creation and file copying with execute function

--- nip/entrypoint.sh
+++ nip/entrypoint.sh
@@ -4,7 +4,10 @@ if [ ! -d ~/.config/nip ]; then
     mkdir -p ~/.config/nip
     cp /nip/config.py ~/.config/nip/config.py
 fi
-mkdir -p ~/.nip/warehouse/nip
 cp /nip/typings.py ~/.config/nip/typings.py
+
+mkdir -p ~/.nip/warehouse/nip
+cp /nip/config.py ~/.nip/warehouse/nip/config.py
+cp /nip/typings.py ~/.nip/warehouse/nip/typings.py
 cp /nip/nip ~/.nip/warehouse/nip/nip
 
