70b8ecff42b7d2cc0a8489a15b359ebd48413bc6
ui3o.com <ui3o.com@gmail.com>
2025-08-07 16:33:05 +0200

    Refactor entrypoint script: update warehouse path to use /nix for consistency

--- nip/entrypoint.sh
+++ nip/entrypoint.sh
@@ -6,8 +6,8 @@ if [ ! -d ~/.config/nip ]; then
 fi
 cp /nip/typings.py ~/.config/nip/typings.py
 
-mkdir -p ~/.nip/warehouse/nip
-cp /nip/config.py ~/.nip/warehouse/nip/config.py
-cp /nip/typings.py ~/.nip/warehouse/nip/typings.py
-cp /nip/nip ~/.nip/warehouse/nip/nip
+mkdir -p /nix/warehouse/nip
+cp /nip/config.py /nix/warehouse/nip/config.py
+cp /nip/typings.py /nix/warehouse/nip/typings.py
+cp /nip/nip /nix/warehouse/nip/nip
 
